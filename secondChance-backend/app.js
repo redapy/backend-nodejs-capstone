7068e95c26ead47db481b48657e961d65bb6a7b6
redapy <baha.redaaaa@gmail.com>
2025-05-03 15:54:05 +0100

    Create API for SecondChanceItems Service

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,23 +1,17 @@
 /*jshint esversion: 8 */
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const pinoLogger = require('./logger');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const secondChanceItemsRoutes = require("./routes/secondChanceItemsRoutes");
 
-const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
+const connectToDatabase = require("./models/db");
 
 const app = express();
-app.use("*",cors());
+app.use("*", cors());
 const port = 3060;
 
 // Connect to MongoDB; we just do this one time
-connectToDatabase().then(() => {
-    pinoLogger.info('Connected to DB');
-})
-    .catch((e) => console.error('Failed to connect to DB', e));
-
+connectToDatabase();
 
 app.use(express.json());
 
@@ -26,15 +20,11 @@ app.use(express.json());
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 //{{insert code here}}
 
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
-//{{insert code here}}
-
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 //{{insert code here}}
 
-
-const pinoHttp = require('pino-http');
-const logger = require('./logger');
+const pinoHttp = require("pino-http");
+const logger = require("./logger");
 
 app.use(pinoHttp({ logger }));
 
@@ -42,23 +32,21 @@ app.use(pinoHttp({ logger }));
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
 //{{insert code here}}
 
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use("/api/secondchance/items", secondChanceItemsRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 //{{insert code here}}
 
-
 // Global Error Handler
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
+  logger.error(err);
+  res.status(500).send("Internal Server Error");
 });
 
-app.get("/",(req,res)=>{
-    res.send("Inside the server")
-})
+app.get("/", (req, res) => {
+  res.send("Inside the server");
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
