9741926e4c0569959c517be1800936ea28c80c34
redapy <baha.redaaaa@gmail.com>
2025-05-04 22:41:45 +0100

    Create API for Search Service

--- secondChance-backend/routes/searchRoutes.js
+++ secondChance-backend/routes/searchRoutes.js
@@ -1,42 +1,44 @@
-const express = require('express');
-const router = express.Router();
-const connectToDatabase = require('../models/db');
-
+const express = require("express");
+const searchRouter = express.Router();
+const connectToDatabase = require("../models/db");
+const collectionName = process.env.MONGO_COLLECTION;
 // Search for gifts
-router.get('/', async (req, res, next) => {
-    try {
-        // Task 1: Connect to MongoDB using connectToDatabase database. Remember to use the await keyword and store the connection in `db`
-        // {{insert code here}}
+searchRouter.get("/", async (req, res, next) => {
+  try {
+    const { name, category, condition, age_years } = req.query;
+    const db = await connectToDatabase();
 
-        const collection = db.collection("gifts");
+    const collection = db.collection(collectionName);
 
-        // Initialize the query object
-        let query = {};
+    // Initialize the query object
+    let query = {};
 
-        // Add the name filter to the query if the name parameter is not empty
-        // if (/* {{insert code here}} */) {
-            query.name = { $regex: req.query.name, $options: "i" }; // Using regex for partial match, case-insensitive
-        // }
-
-        // Task 3: Add other filters to the query
-        if (req.query.category) {
-            // {{insert code here}}
-        }
-        if (req.query.condition) {
-            // {{insert code here}} 
-        }
-        if (req.query.age_years) {
-            // {{insert code here}}
-            query.age_years = { $lte: parseInt(req.query.age_years) };
-        }
-
-        // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
-        // {{insert code here here}}
-
-        res.json(gifts);
-    } catch (e) {
-        next(e);
+    // Add the name filter to the query if the name parameter is not empty
+    if (name && name.trim() !== "") {
+      query.name = { $regex: name, $options: "i" }; // Using regex for partial match, case-insensitive
     }
+
+    // Task 3: Add other filters to the query
+    if (category) {
+      query.category = category;
+    }
+    if (condition) {
+      query.condition = condition;
+    }
+    if (age_years && !isNaN(parseInt(age_years))) {
+      query.age_years = { $lte: parseInt(age_years) };
+    }
+
+    // Task 4: Fetch filtered gifts using the find(query) method. Make sure to use await and store the result in the `gifts` constant
+    const filteredItems = await collection.find(query).toArray();
+    if (!Array.isArray(filteredItems) || filteredItems.length === 0) {
+      return res.status(404).json({ message: "No items found" });
+    }
+
+    res.status(200).json(filteredItems);
+  } catch (e) {
+    next(e);
+  }
 });
 
-module.exports = router;
+module.exports = searchRouter;
