47319ddaea4a6986b186867cf571f9594fe73a49
redapy <baha.redaaaa@gmail.com>
2025-05-05 20:09:15 +0100

    Create login endpoint

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -64,4 +64,50 @@ authRouter.post("/register", async (req, res, next) => {
   }
 });
 
+authRouter.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Username and password are required" });
+    }
+
+    // get users collection
+    const db = await connectToDatabase();
+    const usersCollection = db.collection(collectionName);
+
+    // check if user already exists
+    const user = await usersCollection.findOne({ email });
+
+    logger.info("User found", user);
+
+    if (!user) {
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      logger.error("Passwords do not match");
+      return res.status(404).json({ error: "Wrong pasword" });
+    }
+
+    const { _id, firstName } = user;
+    const payload = {
+      user: {
+        id: _id.toString(),
+      },
+    };
+    const authtoken = jwt.sign(payload, jwtSecret, {
+      expiresIn: JWT_EXPIRES_IN,
+    });
+    res.status(200).json({ authtoken, firstName, email });
+  } catch (e) {
+    logger.error("Error in login route", e);
+    next(e);
+  }
+});
+
 module.exports = authRouter;
